ab0700486eae9ee842f10d38b73fcbbbedd60303
Artyom Torbokov <mail4242@bk.ru>
2024-06-25 13:07:57 +0700

    dwq

--- script.js
+++ script.js
@@ -67,8 +67,8 @@ setTimeout(() => {
     let byteArray = new Uint8Array(byteNumbers);
     
     // Создание Blob из Uint8Array с указанием типа MIME, совместимого с Safari
-    let blob = new Blob([byteArray]);
-    // let blob = new Blob([byteArray], { type: 'audio/mp3' });
+    // let blob = new Blob([byteArray]);
+    let blob = new Blob([byteArray], { type: 'audio/mp3' });
     
     // Создание URL для Blob
     let url = URL.createObjectURL(blob);
