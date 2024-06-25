ba4e1c11322b8788eee806250354ff723878b995
Artyom Torbokov <mail4242@bk.ru>
2024-06-25 11:53:06 +0700

    ff

--- script.js
+++ script.js
@@ -10,18 +10,34 @@ for (let i = 0; i < byteChars.length; i++) {
 // Преобразование массива чисел в Uint8Array
 let byteArray = new Uint8Array(byteNumbers);
 
-// Создание Blob из Uint8Array
-let blob = new Blob([byteArray]);
+// Создание Blob из Uint8Array с указанием типа MIME, совместимого с Safari
+let blob = new Blob([byteArray], { type: 'audio/mp3' });
 
 // Создание URL для Blob
 let url = URL.createObjectURL(blob);
 
 // Создание аудиоэлемента и добавление его на страницу
 const audio = document.createElement('audio');
-audio.innerHTML = ` 
-<source src="${url}" type="audio/mp3">`
+audio.innerHTML = `<source src="${url}" type="audio/mp3">`;
 audio.controls = true;
 
 document.body.appendChild(audio);
+
+// Добавление обработчиков ошибок
+audio.addEventListener('error', (e) => {
+    alert(e)
+    console.error('Ошибка воспроизведения аудио: ', e);
+    document.body.appendChild(e)
+});
+
+// Убедитесь, что пользователь взаимодействует со страницей перед воспроизведением
+document.body.addEventListener('click', () => {
+    audio.play().catch(error => {
+        alert(error)
+        console.error('Ошибка воспроизведения аудио: ', error);
+        document.body.appendChild(error)
+    });
+}, { once: true });  // Слушатель удалится после первого клика
+
 
 
