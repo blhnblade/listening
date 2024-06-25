796e6ec99797cf6aa8707017cf1935fc3f6c7f96
Artyom Torbokov <mail4242@bk.ru>
2024-06-25 12:27:38 +0700

    dw

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ document.querySelector('#play').addEventListener('click', () => {
     // document.body.appendChild(audio);
     
     // Добавление обработчиков ошибок
-    audio.addEventListener('error', (e) => {
-        alert(e)
-        console.error('Ошибка воспроизведения аудио: ', e);
-        let d = document.createElement('div')
-        d.innerHTML = `${e}`
-        document.body.appendChild(d)
-    });
+    // audio.addEventListener('error', (e) => {
+    //     alert(e)
+    //     console.error('Ошибка воспроизведения аудио: ', e);
+    //     let d = document.createElement('div')
+    //     d.innerHTML = `${e}`
+    //     document.body.appendChild(d)
+    // });
 
     audio.play()
     
